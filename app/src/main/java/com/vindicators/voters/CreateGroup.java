0b1ec1c7352f914783817c055298a2da7ccb551f
jonathonderr <jdog.derr1108@gmail.com>
2019-02-17 03:35:55 -0800

    Added Add friends view

--- app/src/main/java/com/vindicators/voters/CreateGroup.java
+++ app/src/main/java/com/vindicators/voters/CreateGroup.java
@@ -8,6 +8,9 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+import android.widget.SearchView;
+
+import java.lang.reflect.Array;
 import java.util.*;
 
 /**
@@ -19,11 +22,13 @@ public class CreateGroup extends AppCompatActivity {
     private RecyclerView recyclerView;
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager layoutManager;
+    public ArrayList<User> users = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_group);
+
         recyclerView = (RecyclerView) findViewById(R.id.my_friends_list);
 
         recyclerView.setHasFixedSize(true);
@@ -32,42 +37,62 @@ public class CreateGroup extends AppCompatActivity {
         recyclerView.setLayoutManager(layoutManager);
 
         DividerItemDecoration dividerItemDecoration= new DividerItemDecoration(recyclerView.getContext(),
-                        layoutManager.getOrientation());
+                layoutManager.getOrientation());
         recyclerView.addItemDecoration(dividerItemDecoration);
 
-        getUsers(new Callback() {
+        final CreateGroupAdapter friendsAdapter = new CreateGroupAdapter(users,CreateGroup.this);
+        recyclerView.setAdapter(friendsAdapter);
+
+        SearchView searchView = findViewById(R.id.searchView);
+        searchView.setQueryHint("Search Friends");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                getUsers(newText, new Callback() {
+                    @Override
+                    public void onCallback(Object value) {
+                        users = (ArrayList<User>) value;
+                        friendsAdapter.updateData(users);
+                    }
+                });
+
+                return false;
+            }
+        });
+
+
+        getUsers("", new Callback() {
             @Override
             public void onCallback(Object value) {
-                ArrayList<User> users =(ArrayList<User>) value;
-                CreateGroupAdapter friendsAdapter = new CreateGroupAdapter(users,CreateGroup.this);
-                recyclerView.setAdapter(friendsAdapter);
+                users = (ArrayList<User>) value;
+                friendsAdapter.updateData(users);
+            }
+        });
+
+
+    }
+
+    private void getUsers(String query, Callback cb) {
+
+        final Callback callback = cb;
+
+        FirebaseServices fHelper = new FirebaseServices();
+        fHelper.searchUsers( query, new Callback() {
+            @Override
+            public void onCallback(Object users) {
+                callback.onCallback(users);
             }
         });
 
     }
 
-    private void getUsers(Callback cb) {
-
-        final Callback callback = cb;
-
-        FirebaseServices fHelper = new FirebaseServices();
-        fHelper.searchUsers("", new Callback() {
-            @Override
-            public void onCallback(Object users) {
-//                User newUser = new User("Bobsyourungle", "Dontnod", "1", null);
-//                users.add(newUser);
-//                users.add(newUser);
-//                users.add(newUser);
-//                users.add(newUser);
-//                users.add(newUser);
-//                users.add(newUser);
-//                users.add(newUser);
-//                users.add(newUser);
-
-                callback.onCallback(users);
-            }
-        });
-
+    public void setUsers(ArrayList<User> users){
+        this.users = users;
     }
 }
 
